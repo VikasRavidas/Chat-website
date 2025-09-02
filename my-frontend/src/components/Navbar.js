91750d6e307e793e9c79dcabc536d97a361d8fee
Vikas  Ravidas <126586254+Vikas123eng@users.noreply.github.com>
2025-09-02 16:20:13 +0530

    fix the responsiveness

--- my-frontend/src/components/Navbar.js
+++ my-frontend/src/components/Navbar.js
@@ -1,118 +1,122 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ClearSearch, logoutUser } from '../actions/auth';
-import gif from './img/chat_gif.gif';
-import images from './img/images.png';
-import search_icon from './img/search_icon.png';
 import searchUsers from '../actions/Search';
 
-class Navbar extends React.Component {
-  searchInputRef = React.createRef();
-  logOut = () => {
-    localStorage.removeItem('token'); // Remove token from storage
-    this.props.dispatch(logoutUser()); // Dispatch logout action
+// Importing icons from the library we just installed
+import { FaUserCircle, FaSearch } from 'react-icons/fa'; 
+
+import gif from './img/chat_gif.gif';
+import images from './img/images.png';
+
+const Navbar = (props) => {
+  // State to manage the dropdown menu's visibility
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const searchInputRef = useRef(null);
+
+  // Function to log the user out
+  const logOut = () => {
+    localStorage.removeItem('token');
+    props.dispatch(logoutUser());
   };
 
-  handleClearSearch = () => {
-    this.props.dispatch(ClearSearch()); // Dispatch clear action
-    if (this.searchInputRef.current) {
-      this.searchInputRef.current.value = ''; // Clear input field
+  // Function to handle search input changes
+  const handleSearch = (e) => {
+    const searchText = e.target.value;
+    props.dispatch(searchUsers(searchText));
+  };
+  
+  // Function to clear search results and input
+  const handleClearSearch = () => {
+    props.dispatch(ClearSearch());
+    if (searchInputRef.current) {
+      searchInputRef.current.value = '';
     }
   };
+  
+  const { auth, results } = props;
 
-  handleSearch = (e) => {
-    const searchText = e.target.value;
+  return (
+    <nav className="nav">
+      {/* Logo */}
+      <div className="left-div">
+        <Link to="/">
+          <img src={gif} alt="logo" />
+        </Link>
+      </div>
 
-    this.props.dispatch(searchUsers(searchText));
-  };
-
-  render() {
-    const { auth, results } = this.props;
-    return (
-      <nav className="nav">
-        {/* Logo */}
-        <div className="left-div">
-          <Link to="/">
-            <img src={gif} alt="logo" />
-          </Link>
+      {/* Search Bar */}
+      {auth.isLoggedin && (
+        <div className="search-container">
+          <FaSearch className="search-icon" />
+          <input
+            placeholder="Search"
+            onChange={handleSearch}
+            ref={searchInputRef}
+          />
+          {results.length > 0 && (
+            <div className="search-results">
+              <ul>
+                {results.map((user) => (
+                  <li
+                    className="search-results-row"
+                    key={user.id}
+                    onClick={handleClearSearch}
+                  >
+                    <Link to={`/user/${user.id}`}>
+                      <img src={images} alt="user-dp" />
+                      <span>{user.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
+      )}
 
-        {/* Search Bar */}
-        {auth.isLoggedin && (
-          <div className="search-container">
-            <img className="search-icon" src={search_icon} alt="search-icon" />
-            <input
-              placeholder="Search"
-              onChange={this.handleSearch}
-              ref={this.searchInputRef}
-            />
-
-            {/* Dummy search results */}
-            {results.length > 0 && (
-              <div className="search-results">
-                <ul>
-                  {results.map((user) => (
-                    <li
-                      className="search-results-row"
-                      key={user.id}
-                      onClick={this.handleClearSearch}
-                    >
-                      <Link to={`/user/${user.id}`}>
-                        <img src={images} alt="user-dp" />
-                        <span>{user.name}</span>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </div>
-        )}
-
-        {/* Right Navigation */}
-        <div className="right-nav">
-          {/* User Profile */}
-          <div className="user">
-            <Link to="/settings">
+      {/* Right Navigation */}
+      <div className="right-nav">
+        {/* User Profile and Dropdown Menu */}
+        <div className="user-profile">
+          {/* This is the icon you click */}
+          <div className="profile-icon-container" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            {auth.isLoggedin ? (
               <img src={images} alt="user-dp" id="user-dp" />
-            </Link>
+            ) : (
+              <FaUserCircle className="guest-icon" />
+            )}
             <span>{auth.isLoggedin ? auth.user.name : 'Guest'}</span>
           </div>
 
-          {/* Navigation Links */}
-          <div className="nav-links">
-            <ul>
-              {!auth.isLoggedin && (
-                <li>
-                  <Link to="/login">Log in</Link>
-                </li>
-              )}
-
-              {auth.isLoggedin && (
-                <li onClick={this.logOut} style={{ cursor: 'pointer' }}>
-                  Log out
-                </li>
-              )}
-
-              {!auth.isLoggedin && (
-                <li>
-                  <Link to="/signup">Register</Link>
-                </li>
-              )}
-            </ul>
-          </div>
+          {/* This is the dropdown menu that appears on click */}
+          {isMenuOpen && (
+            <div className="profile-dropdown">
+              <ul>
+                {auth.isLoggedin ? (
+                  <>
+                    <li><Link to="/settings" onClick={() => setIsMenuOpen(false)}>Settings</Link></li>
+                    <li onClick={logOut}>Log out</li>
+                  </>
+                ) : (
+                  <>
+                    <li><Link to="/login" onClick={() => setIsMenuOpen(false)}>Log In</Link></li>
+                    <li><Link to="/signup" onClick={() => setIsMenuOpen(false)}>Register</Link></li>
+                  </>
+                )}
+              </ul>
+            </div>
+          )}
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
-// Map Redux state to component props
 const mapStateToProps = (state) => ({
   auth: state.auth,
   results: state.search.results,
 });
 
-// Connect Navbar to Redux
 export default connect(mapStateToProps)(Navbar);
